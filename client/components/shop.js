46b91e6aca31c73e46596fd50530eb34d7340247
Katie Agresta <katieagresta1@gmail.com>
2019-08-28 19:46:32 -0400

    adds shopify

--- client/components/shop.js
+++ client/components/shop.js
@@ -1,75 +1,192 @@
 import React from 'react'
 
 class Shop extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      singleShot: true,
-      quantity: 1
-    }
-    this.increase = this.increase.bind(this)
-    this.decrease = this.decrease.bind(this)
-  }
-
-  increase() {
-    this.setState({quantity: this.state.quantity + 1})
-  }
-
-  decrease() {
-    if (!this.state.quantity) {
-      return
-    }
-    this.setState({quantity: this.state.quantity - 1})
-  }
   render() {
     return (
       <div className="shopPage">
         <h1 className="text-center"> our products </h1>
         <img src="/images/line.png" width="100%" />
-        <div className="shopCont">
-          <div className="product"></div>
-          <div className="checkoutCont">
-            <div className="checkoutContent">
-              <div className="checkoutCon">
-                <h2 className="shopHeader">Lemon Lime Relax </h2>
-                <img src="/images/line.png" width="100%" />
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <p>$7.99</p>
-                <div className="shopButtons">
-                  <div>
-                    <button className="shopButtons2 button" type="button">
-                      2oz Single Shot
-                    </button>
-                    <button className=" shopButtons2 button" type="button">
-                      {' '}
-                      2oz / 6 pack
-                    </button>
-                  </div>
-                  <div>
-                    <button
-                      className="shopButtons2 button"
-                      type="button"
-                      onClick={this.decrease}
-                    >
-                      -
-                    </button>
-                    <span> {this.state.quantity} </span>
-                    <button
-                      className="shopButtons2 button"
-                      type="button"
-                      onClick={this.increase}
-                    >
-                      +
-                    </button>
-                  </div>
-                  <button className="shopButtons2 button" type="button">
-                    Add to Cart
-                  </button>
-                </div>
-              </div>
+        <div>
+          <div>
+            <div>
+              <div id="product-component-1567034919011"></div>
+              <div id="product-component-1567035453018"></div>
+              <script type="text/javascript">
+                {(function() {
+                  var scriptURL =
+                    'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js'
+                  if (window.ShopifyBuy) {
+                    if (window.ShopifyBuy.UI) {
+                      ShopifyBuyInit()
+                    } else {
+                      loadScript()
+                    }
+                  } else {
+                    loadScript()
+                  }
+                  function loadScript() {
+                    var script = document.createElement('script')
+                    script.async = true
+                    script.src = scriptURL
+                    ;(
+                      document.getElementsByTagName('head')[0] ||
+                      document.getElementsByTagName('body')[0]
+                    ).appendChild(script)
+                    script.onload = ShopifyBuyInit
+                  }
+                  function ShopifyBuyInit() {
+                    var client = ShopifyBuy.buildClient({
+                      domain: 'getzenya-com.myshopify.com',
+                      storefrontAccessToken: 'fcc0a50ef899f8f8dd6607fa83b2efc6'
+                    })
+                    ShopifyBuy.UI.onReady(client).then(function(ui) {
+                      ui.createComponent('product', {
+                        id: '2196759904307',
+                        node: document.getElementById(
+                          'product-component-1567035453018'
+                        ),
+                        moneyFormat: '%24%7B%7Bamount%7D%7D',
+                        options: {
+                          product: {
+                            styles: {
+                              product: {
+                                '@media (min-width: 601px)': {
+                                  'max-width': '100%',
+                                  'margin-left': '0',
+                                  'margin-bottom': '50px'
+                                },
+                                'text-align': 'left'
+                              },
+                              title: {
+                                'font-family': 'Montserrat, sans-serif',
+                                'font-size': '26px',
+                                color: '#4f6d8c'
+                              },
+                              button: {
+                                'font-family': 'Montserrat, sans-serif',
+                                ':hover': {
+                                  'background-color': '#47627e'
+                                },
+                                'background-color': '#4f6d8c',
+                                ':focus': {
+                                  'background-color': '#47627e'
+                                },
+                                'border-radius': '6px'
+                              },
+                              price: {
+                                'font-family': 'Montserrat, sans-serif',
+                                'font-size': '18px',
+                                color: '#4f6d8c'
+                              },
+                              compareAt: {
+                                'font-family': 'Montserrat, sans-serif',
+                                'font-size': '15.299999999999999px',
+                                color: '#4f6d8c'
+                              },
+                              description: {
+                                'font-family': 'Montserrat, sans-serif',
+                                color: '#4f6d8c'
+                              }
+                            },
+                            layout: 'horizontal',
+                            contents: {
+                              img: false,
+                              imgWithCarousel: true,
+                              button: false,
+                              buttonWithQuantity: true,
+                              description: true
+                            },
+                            width: '100%',
+                            text: {
+                              button: 'Add to cart'
+                            },
+                            googleFonts: ['Montserrat']
+                          },
+                          productSet: {
+                            styles: {
+                              products: {
+                                '@media (min-width: 601px)': {
+                                  'margin-left': '-20px'
+                                }
+                              }
+                            }
+                          },
+                          modalProduct: {
+                            contents: {
+                              img: false,
+                              imgWithCarousel: true,
+                              button: false,
+                              buttonWithQuantity: true
+                            },
+                            styles: {
+                              product: {
+                                '@media (min-width: 601px)': {
+                                  'max-width': '100%',
+                                  'margin-left': '0px',
+                                  'margin-bottom': '0px'
+                                }
+                              },
+                              button: {
+                                'font-family': 'Montserrat, sans-serif',
+                                ':hover': {
+                                  'background-color': '#47627e'
+                                },
+                                'background-color': '#4f6d8c',
+                                ':focus': {
+                                  'background-color': '#47627e'
+                                },
+                                'border-radius': '6px'
+                              }
+                            },
+                            googleFonts: ['Montserrat'],
+                            text: {
+                              button: 'Add to cart'
+                            }
+                          },
+                          cart: {
+                            styles: {
+                              button: {
+                                'font-family': 'Montserrat, sans-serif',
+                                ':hover': {
+                                  'background-color': '#47627e'
+                                },
+                                'background-color': '#4f6d8c',
+                                ':focus': {
+                                  'background-color': '#47627e'
+                                },
+                                'border-radius': '6px'
+                              }
+                            },
+                            text: {
+                              total: 'Subtotal',
+                              button: 'Checkout'
+                            },
+                            googleFonts: ['Montserrat']
+                          },
+                          toggle: {
+                            styles: {
+                              button: {
+                                'font-family': 'Montserrat, sans-serif'
+                              },
+                              toggle: {
+                                'background-color': '#4f6d8c',
+                                ':hover': {
+                                  'background-color': '#47627e'
+                                },
+                                ':focus': {
+                                  'background-color': '#47627e'
+                                }
+                              }
+                            },
+                            googleFonts: ['Montserrat']
+                          }
+                        }
+                      })
+                    })
+                  }
+                })()}
+                ;
+              </script>
             </div>
           </div>
         </div>
